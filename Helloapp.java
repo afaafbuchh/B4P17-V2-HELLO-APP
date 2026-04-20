89341e691ae7fd00b912fa1038ab5a7200e19ab1
Afaaf <afaafbuchh@gmail.com>
2026-04-20 14:14:05 +0530

    Implemented UC4 display hello with multiple names

--- Helloapp.java
+++ Helloapp.java
@@ -1,9 +1,7 @@
 public class Helloapp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello World");
+        for (String name : args) {
+            System.out.println("Hello " + name);
         }
     }
 }
